ea43833bc331e52fee6baf59720653b3e014d15c
carlos5veiga <carlos5veiga@gmail.com>
2022-05-24 07:56:59 -0300

    Database seed

--- src/main/java/com/ex/dscatalog/services/CategoryService.java
+++ src/main/java/com/ex/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.ex.dscatalog.entities.CategoryEntity;
 import com.ex.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -12,6 +13,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<CategoryEntity> findAll() {
         return categoryRepository.findAll();
     }
